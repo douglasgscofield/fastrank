e44cb88de6ba2de29c51dec7a077c4713c00cd04
Douglas G. Scofield <douglasgscofield@gmail.com>
2015-04-30 09:54:36 +0200

    more benchmarking work

--- benchmarking.R
+++ benchmarking.R
@@ -1,13 +1,18 @@
-library(fastrank)
+#library(fastrank)
+devtools::load_all('.')
 library(microbenchmark)
 library(lattice)
+library(RColorBrewer)
 
-vector.length <- as.vector(outer(c(1, 2, 5, 10), c(10, 100, 1000, 10000, 100000)))
+ordermag <- c(10, 100, 1000, 10000, 100000)
+vector.length <- as.vector(outer(c(1, 2, 5, 10), ordermag))
 #vector.length <- as.vector(outer(c(1, 2, 5, 10), c(10)))
 
-sample.fraction = c(10, 5, 2, 1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1)
+micro.times <- rep(rev(ordermag), c(4, 4, 4, 4, 4))
 
-micro.times <- rep(rev(c(10, 100, 1000, 10000, 100000)), c(4, 4, 4, 4, 4))
+#sample.fraction = c(10, 5, 2, 1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1)
+sample.fraction = c(10, 7, 5, 4, 3, 2, 1.7, 1.5, 1.3, 1.2, 
+                    1, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.05)
 
 iterations <- 10
 
@@ -37,11 +42,49 @@ do.benchmark <- function(sort1 = 4L, sort2 = 7L, VL = vector.length, SF = sample
                                       diff = mean(a.diff)))
         }
     }
+    attr(bmark, "sort1") <- sort1
+    attr(bmark, "sort2") <- sort2
+    attr(bmark, "VL") <- VL
+    attr(bmark, "SF") <- SF
+    attr(bmark, "MT") <- MT
+    attr(bmark, "IT") <- IT
     bmark
 }
 
 
-plot.benchmark <- function(b, ...)
+plot.benchmark <- function(b, compress = 0.5, 
+                           min.label = attr(b, "sort1"),
+                           mid.label = "none",
+                           max.label = attr(b, "sort2"),
+                           ...)
 {
-    levelplot(diff ~ length * dupl, data = b, ...)
+    png(file = paste0("diff_", min.label, "_", max.label, ".png"), width = 800, height = 800)
+    opa <- par(mfrow = c(1,1), las = 2, mar = c(4, 4, 1, 1), mgp = c(3, 0.4, 0), tcl = -0.3)
+    b$length = ordered(b$length)
+    main <- paste("orig range", paste(collapse = " ", (signif(range(b$diff), 3))))
+    if (! is.null(compress)) {
+        b$diff <- ifelse(b$diff > compress, compress, b$diff)
+        b$diff <- ifelse(b$diff < -compress, -compress, b$diff)
+        main <- paste(main, "compress", compress)
+    }
+    lvls <- 11
+    zrng <- c(-1, +1) * max(abs(range(b$diff)))
+    zdiff <- as.numeric(cut(c(zrng, b$diff), lvls))
+    zpalette <- brewer.pal(lvls, "Spectral")
+    #zpalette <- colorRampPalette(c("red","blue"))(lvls)
+    zcol <- zpalette[zdiff]
+    plot.default(b$length, b$dupl, ylim = c(0, 1), axes = FALSE,
+                 pch = 19, cex = 1.5, cex.lab = 1, col = zcol,
+                 xlab = "Vector length", ylab = "Value duplicate fraction",
+                 main = main) 
+    axis(1, at = unique(sort(as.numeric(b$length))), labels = levels(b$length))
+    axis(2)
+    leg <- c(zrng[1], 0, zrng[2])
+    leg <- c(min.label, mid.label, max.label)
+    legend("top", legend = leg, pch = 19, pt.cex = 1.5,
+           col = c(zpalette[1], zpalette[(lvls + 1)/2], zpalette[lvls]),
+           horiz = TRUE, bty = "n")
+    #plot(b$length, b$repet, pch = 19, cex = 1.5, col = zcol) 
+    par(opa)
+    dev.off()
 }
